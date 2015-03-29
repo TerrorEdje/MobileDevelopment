31aa7c7c59d89b3e0d7491d6ebfd6acdae7a7b58
Edwin Hattink <edwinhattink@me.com>
2015-03-29 19:10:44 +0200

    Update

--- ClassMate/routes/courses.js
+++ ClassMate/routes/courses.js
@@ -156,7 +156,7 @@ router.route('/:id/').put(function(req, res) {
 });
 
 /* POST course */
-router.route('/:id/').post(function(req, res) {
+router.route('/').post(function(req, res) {
   	var course = new Course(req.body);
   	course.save(function(err) {
   		if (err)
