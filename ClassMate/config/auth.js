5e6303c164b93fd3ae53b726f9cc3fe9074c6b5a
Yorick <yorickvanklinken@gmail.com>
2015-05-16 15:28:12 +0200

    yet another commit

--- ClassMate/config/auth.js
+++ ClassMate/config/auth.js
@@ -2,6 +2,7 @@ module.exports = {
 	'googleAuth' : {
         'clientID'      : '132410763562-ittomvev77dqbi2a9p1kbe4jkahnikvb.apps.googleusercontent.com',
         'clientSecret'  : 'wt_T8e61-lC9_px35QIJo3Ek',
-        'callbackURL'   : 'http://localhost:3000/auth/google/callback'
+        //'callbackURL'   : 'http://localhost:3000/auth/google/callback'
+        'callbackURL'   : '/auth/google/callback'
     }
 };
